af2902c775e9fb0faf21721ff3725edc767d05c5
Adwait Rao <adwaitrao7000@gmail.com>
2025-01-01 18:52:19 +0530

    added assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <p>Hello world</p>
-    </>
-  );
+  return <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64"></div>;
 }
 
 export default App;
